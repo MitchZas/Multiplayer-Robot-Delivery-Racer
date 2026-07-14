c2255c8cf966087d6867f82e986db911b57aaa80
MitchZas <mitchellzasa@gmail.com>
2026-07-14 09:03:33 -0400

    Healthbar is implemented and working correctly

--- MultiplayerRobotDeliveryRacer/Assets/Scripts/HealthBar.cs
+++ MultiplayerRobotDeliveryRacer/Assets/Scripts/HealthBar.cs
@@ -6,17 +6,23 @@ public class HealthBar : MonoBehaviour
 
    public Slider slider;
 
+   public Gradient gradient;
+    public Image fill;
+
    public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
         Debug.Log("SetMaxHealth called with: " + health + " | slider.maxValue is now: " + slider.maxValue);
 
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
         Debug.Log("SetHealth called with: " + health);
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
